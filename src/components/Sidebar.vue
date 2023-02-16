a4f02072626d2a43a71ddbcd00f07c5eb85f43b1
marioalvarez32 <33704981+marioalvarez32@users.noreply.github.com>
2023-02-15 21:25:22 -0600

    Making links easier to see

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -3,12 +3,7 @@
     <v-navigation-drawer permanent rounded="lg" rail elevation="5">
       <v-list :lines="false" density="compact" nav class="sidebar__nav-list">
         <template v-for="(menuItem, i) in navigationItems" :key="i">
-          <v-list-item
-            v-if="!menuItem.SubItems"
-            :to="menuItem.Path"
-            :active="$route.path === menuItem.Path"
-            active-color="primary"
-          >
+          <v-list-item v-if="!menuItem.SubItems" :to="menuItem.Path" :active="$route.path === menuItem.Path" active-color="primary">
             <template v-slot:prepend>
               <v-icon :icon="menuItem.Icon"></v-icon>
             </template>
@@ -17,30 +12,13 @@
           </v-list-item>
           <v-list-group v-else value="Tools">
             <template v-slot:activator="{ props }">
-              <v-list-item
-                v-bind="props"
-                :prepend-icon="menuItem.Icon"
-                :active="$route.path === subItem.Path"
-                :title="menuItem.Text"
-              ></v-list-item>
+              <v-list-item v-bind="props" :prepend-icon="menuItem.Icon" :active="$route.path === subItem.Path" :title="menuItem.Text"></v-list-item>
             </template>
-            <v-list-item
-              v-for="(subItem, i) in menuItem.SubItems"
-              :to="subItem.Path"
-              :key="i"
-              :value="subItem.Text"
-              :title="subItem.Text"
-              :prepend-icon="subItem.Icon"
-            >
-            </v-list-item>
+            <v-list-item v-for="(subItem, i) in menuItem.SubItems" :to="subItem.Path" :key="i" :value="subItem.Text" :title="subItem.Text" :prepend-icon="subItem.Icon"> </v-list-item>
           </v-list-group>
         </template>
         <div class="sidebar__setting-icon-wrapper">
-          <v-list-item
-            :to="settingNav.Path"
-            :active="$route.path === settingNav.Path"
-            active-color="primary"
-          >
+          <v-list-item :to="settingNav.Path" :active="$route.path === settingNav.Path" active-color="primary">
             <template v-slot:prepend>
               <v-icon :icon="settingNav.Icon"></v-icon>
             </template>
@@ -99,5 +77,12 @@ export default {
   display: flex;
   align-items: flex-end;
   height: 100%;
+}
+
+:deep(.v-list.v-list--nav a) {
+  color: #000000 !important;
+}
+:deep(.v-list.v-list--nav a.text-primary) {
+  color: #ffffff !important;
 }
 </style>
