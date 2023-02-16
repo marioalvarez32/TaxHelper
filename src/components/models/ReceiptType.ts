51c03768014f0e7e6d64affa2058f1d1e553a937
marioalvarez32 <33704981+marioalvarez32@users.noreply.github.com>
2023-02-15 21:20:38 -0600

    Adding models for the receipts and also the XML file.

---
+++ src/components/models/ReceiptType.ts
@@ -0,0 +1,23 @@
+export default class ReceiptType {
+  UUID: string;
+  IssuerRfc: string;
+  IssuerName: string;
+  ReceiverRfc: string;
+  ReceiverName: string;
+  SubTotal: number;
+  Total: number;
+  TaxAmount: number;
+  TaxPercentage: string;
+
+  constructor(receipt: any) {
+    this.IssuerName = receipt.IssuerName;
+    this.IssuerRfc = receipt.IssuerRfc;
+    this.ReceiverRfc = receipt.ReceiverRfc;
+    this.ReceiverName = receipt.ReceiverName;
+    this.SubTotal = parseFloat(receipt.SubTotal);
+    this.Total = parseFloat(receipt.Total);
+    this.TaxAmount = parseFloat(receipt.TaxAmount);
+    this.TaxPercentage = receipt.TaxPercentage;
+    this.UUID = receipt.UUID;
+  }
+}
---
+++ src/components/models/ReceiptXmlType.ts
@@ -0,0 +1,97 @@
+import ReceiptType from './ReceiptType';
+
+export default class ReceiptXmlType {
+  UUID: string;
+  ReceiptSummary: {
+    Certificate: string;
+    Date: string;
+    Folio: string;
+    PaymentMethod: number;
+    Currency: string;
+    CertificateNumber: string;
+    StampId: string;
+    Series: string;
+    SubTotal: string;
+    ExchangeType: string;
+    Total: string;
+    ReceiptType: string;
+    ReceiptVersion: string;
+  };
+  Taxes: {
+    Amount: string;
+    TaxType: string;
+    TaxPercentage: string;
+    TaxFactor: string;
+  };
+  Receiver: {
+    Name: string;
+    TaxRegime: string;
+    Rfc: string;
+  };
+  Issuer: {
+    Name: string;
+    TaxRegime: string;
+    Rfc: string;
+  };
+
+  constructor(xmlData: any) {
+    console.log('🚀 ~ file: ReceiptXmlType.ts:31 ~ ReceiptXmlType ~ constructor ~ xmlData', xmlData);
+    const data = xmlData['cfdi:Comprobante'];
+    const summaryData = data['$'];
+    this.ReceiptSummary = {
+      Certificate: summaryData['Certificado'],
+      Date: summaryData['Fecha'],
+      Folio: summaryData['Folio'],
+      PaymentMethod: summaryData['FormaPago'],
+      Currency: summaryData['MetodoPago'],
+      CertificateNumber: summaryData['NoCertificado'],
+      StampId: summaryData['Sello'],
+      Series: summaryData['Serie'],
+      SubTotal: summaryData['SubTotal'],
+      ExchangeType: summaryData['TipoDeCambio'],
+      Total: summaryData['Total'],
+      ReceiptType: summaryData['TipoDeComprobante'],
+      ReceiptVersion: summaryData['Version'],
+    };
+    const taxInformation = data['cfdi:Conceptos']['0']['cfdi:Concepto'][0]['cfdi:Impuestos'][0]['cfdi:Traslados'][0]['cfdi:Traslado'][0]['$'];
+    this.Taxes = {
+      Amount: taxInformation['Importe'],
+      TaxType: taxInformation['Impuesto'],
+      TaxPercentage: taxInformation['TasaOCuota'],
+      TaxFactor: taxInformation['TipFactor'],
+    };
+    const receiver = data['cfdi:Receptor']['0']['$'];
+    this.Receiver = {
+      Name: receiver['Nombre'],
+      TaxRegime: receiver['RegimenFiscal'],
+      Rfc: receiver['Rfc'],
+    };
+    const issuer = data['cfdi:Emisor']['0']['$'];
+    this.Issuer = {
+      Name: issuer['Nombre'],
+      TaxRegime: issuer['RegimenFiscal'],
+      Rfc: issuer['Rfc'],
+    };
+    const complementaryData = data['cfdi:Complemento'][0]['tfd:TimbreFiscalDigital'][0]['$'];
+    this.UUID = complementaryData.UUID;
+  }
+
+  convertToReceiptType(): ReceiptType {
+    const { SubTotal, Total } = this.ReceiptSummary;
+    const { Amount: TaxAmount, TaxPercentage } = this.Taxes;
+    const { Name: ReceiverName, Rfc: ReceiverRfc } = this.Receiver;
+    const { Name: IssuerName, Rfc: IssuerRfc } = this.Issuer;
+
+    return new ReceiptType({
+      IssuerName,
+      IssuerRfc,
+      ReceiverRfc,
+      ReceiverName,
+      SubTotal,
+      Total,
+      TaxAmount,
+      TaxPercentage,
+      UUID: this.UUID,
+    });
+  }
+}
