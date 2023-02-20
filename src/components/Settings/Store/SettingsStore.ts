a075171cd3e234c9044a0d2cb6186088958474f9
marioalvarez32 <33704981+marioalvarez32@users.noreply.github.com>
2023-02-19 22:36:42 -0600

    Created a store for the settings.

---
+++ src/components/Settings/Store/SettingsStore.ts
@@ -0,0 +1,64 @@
+import { defineStore } from 'pinia';
+import AppSettings from '../Models/AppSettings';
+import SettingsGroup from '../Models/SettingsGroup';
+import GeneralSettings from '../GeneralSettings.vue';
+import { Component } from 'vue';
+
+interface SettingsStore {
+  userSettings: AppSettings;
+  selectedSettingGroup: string;
+}
+export const useSettingsStore = defineStore('Settings', {
+  state: (): SettingsStore => ({
+    userSettings: {
+      Interface: {
+        Name: 'interface-settings',
+        Icon: 'mdi-application-settings-outline',
+        FriendlyName: 'Interface',
+        Label: 'Interface',
+        Description: 'All settings pertaining to the User Interface.',
+        SettingsItemGroups: [
+          {
+            Name: 'interface-theme',
+            Label: 'Application Theme',
+            Description: 'Customize the look and feel of the application',
+            SettingItems: [
+              {
+                Name: 'theme',
+                Label: 'Interface Theme',
+                Description: 'Select a light or dark theme for the application',
+                DefaultValue: 'dark',
+                Value: 'dark',
+              },
+              {
+                Name: 'navbar-theme',
+                Label: 'Navigation Bar Color',
+                Description: 'Select the navigation bar color',
+                DefaultValue: 'lightGray',
+                Value: 'lightGray',
+              },
+            ],
+          },
+        ],
+      },
+    },
+    selectedSettingGroup: 'interface-settings',
+  }),
+  getters: {
+    getSelectedSettingGroup(state): SettingsGroup {
+      switch (state.selectedSettingGroup) {
+        case state.userSettings.Interface.Name:
+        default:
+          return state.userSettings.Interface;
+      }
+    },
+    getSelectedSettingPageComponent(state): Component {
+      switch (state.selectedSettingGroup) {
+        case state.userSettings.Interface.Name:
+        default:
+          return GeneralSettings;
+      }
+    },
+  },
+  actions: {},
+});
