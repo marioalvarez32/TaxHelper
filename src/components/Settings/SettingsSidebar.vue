0f9b5092aef9d2823650c512c745497e756f5de7
marioalvarez32 <33704981+marioalvarez32@users.noreply.github.com>
2023-02-19 20:23:01 -0600

    Adding the navigation drawer for the settings page to be able to select different settings sub-pages.

---
+++ src/components/Settings/SettingsSidebar.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <v-navigation-drawer class="settings__navigation">
+      <h4>Settings</h4>
+      <v-list density="compact">
+        <v-list-item active-color="primary">
+          <template v-slot:prepend>
+            <v-icon icon="mdi-palette"></v-icon>
+          </template>
+
+          <v-list-item-title v-text="`Interface`"></v-list-item-title>
+        </v-list-item>
+        <v-list-item active-color="primary">
+          <template v-slot:prepend>
+            <v-icon icon="mdi-palette"></v-icon>
+          </template>
+
+          <v-list-item-title v-text="`Interface`"></v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  setup() {
+    return {};
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+:deep(.v-navigation-drawer__content) {
+  margin: 25px;
+}
+</style>
