ca768cb204b6ee820392f12e2a5fe6a4a3df1718
marioalvarez32 <33704981+marioalvarez32@users.noreply.github.com>
2023-02-19 20:22:28 -0600

    Adding the settings page.

---
+++ src/views/Settings.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-layout full-height>
+    <SettingsSidebar />
+    <v-main style="min-height: 300px"></v-main>
+  </v-layout>
+</template>
+
+<script lang="ts">
+import SettingsSidebar from '@/components/Settings/SettingsSidebar.vue';
+
+export default {
+  components: {
+    SettingsSidebar,
+  },
+  setup() {
+    return {};
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
