af9351c174f157ef0fb9f758faa849141d609509
marioalvarez32 <33704981+marioalvarez32@users.noreply.github.com>
2023-02-20 21:59:37 -0600

    Adding an inital layout fort he general settings page.

--- src/components/Settings/GeneralSettings.vue
+++ src/components/Settings/GeneralSettings.vue
@@ -1,11 +1,39 @@
 <template>
-  <div>hello world</div>
+  <div class="general-settings">
+    <SettingsItemGroup title="Theme" description="Personalize your experience by selecting a different color or selecting a dark or light theme.">
+      <template #settings>
+        <SettingItem settingType="predefined-color-selection" title="Application Theme" description="Select a dark or light theme" />
+        <SettingItem settingType="predefined-color-selection" title="Application Theme" description="Select a dark or light theme" />
+        <SettingItem settingType="predefined-color-selection" title="Application Theme" description="Select a dark or light theme" />
+        <SettingItem settingType="predefined-color-selection" title="Application Theme" description="Select a dark or light theme" />
+        <SettingItem settingType="predefined-color-selection" title="Application Theme" description="Select a dark or light theme" />
+        <SettingItem settingType="predefined-color-selection" title="Application Theme" description="Select a dark or light theme" />
+        <SettingItem settingType="predefined-color-selection" title="Application Theme" description="Select a dark or light theme" />
+        <SettingItem settingType="predefined-color-selection" title="Application Theme" description="Select a dark or light theme" />
+      </template>
+    </SettingsItemGroup>
+    <v-divider></v-divider>
+  </div>
 </template>
 
-<script>
+<script lang="ts">
+import { toRefs } from 'vue';
+import SettingsItemGroup from '../Settings/Components/SettingsItemGroup.vue';
+import SettingItem from '../Settings/Components/SettingItem.vue';
+import SettingsGroup from './Models/SettingsGroup';
+import { useSettingsStore } from './Store/SettingsStore';
+
 export default {
+  components: {
+    SettingsItemGroup,
+    SettingItem,
+  },
   setup() {
-    return {};
+    const settingsStore = useSettingsStore();
+    const { userSettings, selectedSettingGroup, getSelectedSettingGroup: settings, getSelectedSettingPageComponent } = toRefs(settingsStore);
+    return {
+      settings,
+    };
   },
 };
 </script>
