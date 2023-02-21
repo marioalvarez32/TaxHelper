6c0a43e406a48fe92b0f7d376c7022cc0b7f8f31
marioalvarez32 <33704981+marioalvarez32@users.noreply.github.com>
2023-02-20 22:00:00 -0600

    Adding components to represent a setting item and a group.

---
+++ src/components/Settings/Components/SettingItem.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="setting-item">
+    <div class="setting-item__input">
+      <v-switch value="true" label="Theme" inset color="primary" true-value="yes" false-value="no" hide-details></v-switch>
+    </div>
+    <div class="setting-item__description">
+      <p class="setting-item__description-title">{{ title }}</p>
+      <p>{{ description }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    settingType: {
+      type: String,
+      required: false,
+    },
+  },
+  setup() {
+    return {};
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.setting-item {
+  display: flex;
+  gap: 30px;
+  align-items: center;
+}
+
+.setting-item__description {
+  flex-basis: 35%;
+
+  .setting-item__description-title {
+    font-size: 0.85rem;
+    font-weight: 600;
+  }
+  p {
+    font-size: 0.8rem;
+  }
+}
+
+.setting-item__items-container {
+  flex-basis: 65%;
+}
+</style>
---
+++ src/components/Settings/Components/SettingsItemGroup.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="setting-item-group">
+    <div class="setting-item-group__description">
+      <p class="setting-item-group__description-title">{{ title }}</p>
+      <p>{{ description }}</p>
+    </div>
+    <div class="setting-item-group__items-container">
+      <slot name="settings"></slot>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { toRefs } from 'vue';
+import { useSettingsStore } from '../Store/SettingsStore';
+import SettingsGroup from '../Models/SettingsGroup';
+import SettingsItemGroups from '../Models/SettingsItemGroups';
+
+export default {
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+  },
+  setup() {
+    return {};
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.setting-item-group {
+  display: flex;
+  gap: 30px;
+  margin: 25px 0;
+}
+
+.setting-item-group__description {
+  flex-basis: 35%;
+
+  .setting-item-group__description-title {
+    font-size: 1rem;
+    font-weight: 600;
+  }
+  p {
+    font-size: 0.9rem;
+  }
+}
+
+.setting-item-group__items-container {
+  flex-basis: 65%;
+}
+</style>
