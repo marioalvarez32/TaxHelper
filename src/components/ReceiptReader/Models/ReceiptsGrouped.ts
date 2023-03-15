d46db87b818a1b4271377da9245b2ce721103f0c
marioalvarez32 <33704981+marioalvarez32@users.noreply.github.com>
2023-03-14 22:02:53 -0500

    Added a model for the grouped table view.

---
+++ src/components/ReceiptReader/Models/ReceiptsGrouped.ts
@@ -0,0 +1,20 @@
+import ReceiptType from '../../models/ReceiptType';
+
+export default class ReceiptsGrouped {
+  Key: string;
+  IssuerRfc: string;
+  IssuerName: string;
+  SubTotal: number;
+  Total: number;
+  TaxAmount: number;
+  Receipts: ReceiptType[];
+
+  constructor(receipt: any) {
+    this.IssuerName = receipt.IssuerName;
+    this.IssuerRfc = receipt.IssuerRfc;
+    this.SubTotal = 0;
+    this.Total = 0;
+    this.TaxAmount = 0;
+    this.Receipts = [];
+  }
+}
