f90a91aa0ac99d294a468191799d469ebc38303c
marioalvarez32 <33704981+marioalvarez32@users.noreply.github.com>
2023-03-14 22:02:35 -0500

    Added the table component and created a second view that groups data by RFC Issuer.

---
+++ src/components/ReceiptReader/Components/ReceiptsTable.vue
@@ -0,0 +1,100 @@
+<template>
+  <div class="receipt-reader__table-container">
+    <v-overlay persistent :model-value="isExportingData" contained>
+      <v-progress-circular :size="75" color="primary" indeterminate></v-progress-circular>
+    </v-overlay>
+    <v-table v-if="selectedTableView == 'default'" density="compact" fixed-header>
+      <thead>
+        <tr>
+          <th class="text-left">Issuer</th>
+          <th class="text-left">Issuer RFC</th>
+          <th class="text-left">Receiver</th>
+          <th class="text-left">Sub Total</th>
+          <th class="text-left">Total</th>
+          <th class="text-left">Tax Amount</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="file in addedReceipts" :key="file.UUID">
+          <td>{{ file.IssuerName }}</td>
+          <td>{{ file.IssuerRfc }}</td>
+          <td>{{ file.ReceiverName }}</td>
+          <td>${{ file.SubTotal }}</td>
+          <td>${{ file.Total }}</td>
+          <td>${{ file.TaxAmount }}</td>
+        </tr>
+      </tbody>
+    </v-table>
+    <v-table v-if="selectedTableView == 'grouped-by-issuer-RFC'" density="compact" fixed-header>
+      <thead>
+        <tr>
+          <th class="text-left">Issuer</th>
+          <th class="text-left">Issuer RFC</th>
+          <th class="text-left"># of Receipts</th>
+          <th class="text-left">Sub Total</th>
+          <th class="text-left">Total</th>
+          <th class="text-left">Tax Amount</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="group in receiptsByIssuerRFC" :key="group.Key">
+          <td>{{ group.IssuerName }}</td>
+          <td>{{ group.IssuerRfc }}</td>
+          <td>{{ group.Receipts.length }}</td>
+          <td>${{ group.SubTotal }}</td>
+          <td>${{ group.Total }}</td>
+          <td>${{ group.TaxAmount }}</td>
+        </tr>
+      </tbody>
+    </v-table>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref, computed } from 'vue';
+import ReceiptType from '../../models/ReceiptType';
+import { PropType } from 'vue';
+import ReceiptsGrouped from '../Models/ReceiptsGrouped';
+
+export default {
+  components: {},
+  props: {
+    addedReceipts: {
+      type: Array as PropType<ReceiptType[]>,
+      default: [],
+    },
+    isExportingData: {
+      type: Boolean,
+      default: false,
+    },
+    selectedTableView: {
+      type: String,
+      default: 'default',
+    },
+  },
+  setup(props) {
+    const receiptsByIssuerRFC = computed<ReceiptsGrouped[]>(() => groupBy(Array.from(props.addedReceipts), 'IssuerRfc'));
+
+    function groupBy(array, key) {
+      return array.reduce((result, obj: ReceiptType) => {
+        if (!result[obj[key]]) {
+          result[obj[key]] = new ReceiptsGrouped(obj);
+        }
+        const group = result[obj[key]];
+        group.Key = key;
+        group.SubTotal += obj.SubTotal;
+        group.Total += obj.Total;
+        group.TaxAmount += obj.TaxAmount;
+        group.Receipts.push(obj);
+        return result;
+      }, {});
+    }
+
+    return {
+      receiptsByIssuerRFC,
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
