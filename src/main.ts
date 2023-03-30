6bd9b887481b64a682885384d144efbe498128ad
marioalvarez32 <33704981+marioalvarez32@users.noreply.github.com>
2023-03-29 20:09:49 -0500

    Reducing the scope of this and removing internalization.

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,6 @@ import { router } from './router';
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
 import { vuetify } from './vuetify';
-import './localization';
-import I18NextVue from 'i18next-vue';
-import i18next from 'i18next';
 
 import App from './App.vue';
 
@@ -17,7 +14,6 @@ const app = createApp(App);
 app.use(router);
 app.use(pinia);
 app.use(vuetify);
-app.use(I18NextVue, { i18next });
 
 void app.mount('#app').$nextTick(() => {
   postMessage({ payload: 'removeLoading' }, '*');
