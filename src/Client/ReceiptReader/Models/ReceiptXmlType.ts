376795fb945a21efef1b1bd28bc773093b3d456e
marioalvarez32 <33704981+marioalvarez32@users.noreply.github.com>
2023-12-30 18:58:05 -0600

    Adding validation and default values to the xml receipts since the required fields are not known.

--- src/Client/ReceiptReader/Models/ReceiptXmlType.ts
+++ src/Client/ReceiptReader/Models/ReceiptXmlType.ts
@@ -34,39 +34,43 @@ export default class ReceiptXmlType {
 
   constructor(xmlData: any) {
     const data = xmlData['cfdi:Comprobante'];
-    const summaryData = data['$'];
+    const summaryData = data['$'] ?? {};
     this.ReceiptSummary = {
-      Certificate: summaryData['Certificado'],
-      Date: summaryData['Fecha'],
-      Folio: summaryData['Folio'],
-      PaymentMethod: summaryData['FormaPago'],
-      Currency: summaryData['MetodoPago'],
-      CertificateNumber: summaryData['NoCertificado'],
-      StampId: summaryData['Sello'],
-      Series: summaryData['Serie'],
-      SubTotal: summaryData['SubTotal'],
-      ExchangeType: summaryData['TipoDeCambio'],
-      Total: summaryData['Total'],
-      ReceiptType: summaryData['TipoDeComprobante'],
-      ReceiptVersion: summaryData['Version'],
-      Descuento: summaryData['Descuento'],
+      Certificate: summaryData['Certificado'] ?? 'Sin certificado',
+      Date: summaryData['Fecha'] ?? 'Sin Fecha',
+      Folio: summaryData['Folio'] ?? 'Sin Folio',
+      PaymentMethod: summaryData['FormaPago'] ?? 0,
+      Currency: summaryData['MetodoPago'] ?? 'Sin moneda',
+      CertificateNumber: summaryData['NoCertificado'] ?? 'Sin # de certificado',
+      StampId: summaryData['Sello'] ?? 'Sin sello',
+      Series: summaryData['Serie'] ?? 'Sin serie',
+      SubTotal: summaryData['SubTotal'] ?? 0,
+      ExchangeType: summaryData['TipoDeCambio'] ?? 'Sin tipo de cambio',
+      Total: summaryData['Total'] ?? 0,
+      ReceiptType: summaryData['TipoDeComprobante'] ?? 'Sin tipo de comprobante',
+      ReceiptVersion: summaryData['Version'] ?? 'Sin versión',
+      Descuento: summaryData['Descuento'] ?? 0,
     };
-    const totalTax = data['cfdi:Impuestos']['0']['$'];
+    const totalTax = data['cfdi:Impuestos']['0']['$'] ?? {};
     this.Taxes = {
-      Amount: totalTax['TotalImpuestosTrasladados'],
+      Amount: totalTax['TotalImpuestosTrasladados'] ?? 0,
     };
-    const receiver = data['cfdi:Receptor']['0']['$'];
+
+    const receiver = data['cfdi:Receptor']['0']['$'] ?? {};
+
     this.Receiver = {
-      Name: receiver['Nombre'],
-      TaxRegime: receiver['RegimenFiscal'],
-      Rfc: receiver['Rfc'],
+      Name: receiver['Nombre'] ?? 'Sin nombre de receptor',
+      TaxRegime: receiver['RegimenFiscal'] ?? 'Sin régimen fiscal',
+      Rfc: receiver['Rfc'] ?? 'Sin RFC de receptor',
     };
-    const issuer = data['cfdi:Emisor']['0']['$'];
+
+    const issuer = data['cfdi:Emisor']['0']['$'] ?? {};
     this.Issuer = {
-      Name: issuer['Nombre'],
-      TaxRegime: issuer['RegimenFiscal'],
-      Rfc: issuer['Rfc'],
+      Name: issuer['Nombre'] ?? 'Sin nombre de emisor',
+      TaxRegime: issuer['RegimenFiscal'] ?? 'Sin régimen fiscal',
+      Rfc: issuer['Rfc'] ?? 'Sin RFC de emisor',
     };
+
     const complementaryData = data['cfdi:Complemento'][0]['tfd:TimbreFiscalDigital'][0]['$'];
     this.UUID = complementaryData.UUID;
   }
