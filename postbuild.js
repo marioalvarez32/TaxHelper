2758728b93a3dade617e9e313a374cc56ebb9a84
marioalvarez32 <33704981+marioalvarez32@users.noreply.github.com>
2023-12-30 23:36:17 -0600

    Another try.

--- postbuild.js
+++ postbuild.js
@@ -1,10 +1,14 @@
 const { glob } = require('glob');
 const { rimraf } = require('rimraf');
 
-glob('release/*').then((files) => {
-  files.forEach((file) => {
-    if ((!file.includes('Tax-Helper-Setup') && !file.endsWith('exe')) || file.endsWith('blockmap')) {
-      rimraf.sync(file);
-    }
+const start = async () => {
+  await glob('release/*').then((files) => {
+    files.forEach((file) => {
+      if ((!file.includes('Tax-Helper-Setup') && !file.endsWith('exe')) || file.endsWith('blockmap')) {
+        rimraf.sync(file);
+      }
+    });
   });
-});
+};
+
+start();
