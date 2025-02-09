3ebd5ec9f1d793053819c669427f45f7a898b7be
marioalvarez32 <33704981+marioalvarez32@users.noreply.github.com>
2025-02-09 01:04:52 -0600

    refactor: Fixing issues with the npm build script.

--- postbuild.js
+++ postbuild.js
@@ -1,5 +1,11 @@
-const fs = require('fs');
-const path = require('path');
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 
 const directory = path.resolve(__dirname, 'release');
 
