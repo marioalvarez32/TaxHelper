2aa56a6b18b8a901022d897f4dd4154e0669315c
marioalvarez32 <33704981+marioalvarez32@users.noreply.github.com>
2025-06-27 21:09:37 -0500

    Better formatting and linting the vite config file.

--- eslintrc.js
+++ eslintrc.js
@@ -1,15 +1,15 @@
 module.exports = {
-  env: {
-    node: true,
-  },
-  parser: 'vue-eslint-parser',
-  extends: ['eslint:recommended', 'plugin:vue/vue3-recommended', 'prettier'],
-  rules: {
-    // override/add rules settings here, such as:
-    // 'vue/no-unused-vars': 'error'
-  },
-  parserOptions: {
-    parser: '@babel/eslint-parser',
-    sourceType: 'module',
-  },
+	env: {
+		node: true,
+	},
+	parser: 'vue-eslint-parser',
+	extends: ['eslint:recommended', 'plugin:vue/vue3-recommended', 'prettier'],
+	rules: {
+		// override/add rules settings here, such as:
+		// 'vue/no-unused-vars': 'error'
+	},
+	parserOptions: {
+		parser: '@babel/eslint-parser',
+		sourceType: 'module',
+	},
 };
--- vite.config.ts
+++ vite.config.ts
@@ -1,120 +1,120 @@
-import { rmSync } from 'node:fs'
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import electron from 'vite-plugin-electron'
-import renderer from 'vite-plugin-electron-renderer'
-import pkg from './package.json'
-import path from 'path'
-import { fileURLToPath, URL } from 'url'
-import { dirname } from 'path'
+import { rmSync } from 'node:fs';
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import electron from 'vite-plugin-electron';
+import renderer from 'vite-plugin-electron-renderer';
+import pkg from './package.json';
+import path from 'path';
+import { fileURLToPath, URL } from 'url';
+import { dirname } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command, mode }) => {
-  const __filename = fileURLToPath(import.meta.url)
-  const __dirname = dirname(__filename)
-  
-  // Clean dist on fresh builds
-  if (command === 'build') {
-    rmSync('dist-electron', { recursive: true, force: true })
-  }
+	const __filename = fileURLToPath(import.meta.url);
+	const __dirname = dirname(__filename);
 
-  const isServe = command === 'serve'
-  const isBuild = command === 'build'
-  const isDev = mode === 'development'
-  const sourcemap = isServe || !!process.env.VSCODE_DEBUG
+	// Clean dist on fresh builds
+	if (command === 'build') {
+		rmSync('dist-electron', { recursive: true, force: true });
+	}
 
-  return {
-    plugins: [
-      vue(),
-      electron([
-        {
-          // Main-Process entry file of the Electron App.
-          entry: 'electron/main/index.ts',
-          onstart(options) {
-            if (process.env.VSCODE_DEBUG) {
-              console.log('[startup] Electron App')
-            } else {
-              options.startup()
-            }
-          },
-          vite: {
-            build: {
-              sourcemap,
-              minify: isBuild,
-              outDir: path.resolve(__dirname, 'dist-electron/main'),
-              rollupOptions: {
-                external: Object.keys(pkg.dependencies ?? {}),
-              },
-            },
-            optimizeDeps: {
-              exclude: ['electron']
-            }
-          },
-        },
-        {
-          entry: 'electron/preload/index.ts',
-          onstart(options) {
-            // Notify the Renderer-Process to reload the page when the Preload-Scripts build is complete,
-            // instead of restarting the entire Electron App.
-            options.reload()
-          },
-          vite: {
-            build: {
-              sourcemap: sourcemap ? 'inline' : undefined,
-              minify: isBuild,
-              outDir: 'dist-electron/preload',
-              rollupOptions: {
-                external: Object.keys('dependencies' in pkg ? pkg.dependencies : {}),
-              },
-            },
-            optimizeDeps: {
-              exclude: ['electron']
-            }
-          },
-        },
-      ]),
-      // Use Node.js API in the Renderer-process
-      renderer({
-        nodeIntegration: true,
-      }),
-    ],
-    resolve: {
-      alias: {
-        '@': path.resolve(__dirname, './src'),
-        'Views': fileURLToPath(new URL('./src/views', import.meta.url)),
-        'Components': fileURLToPath(new URL('./src/components', import.meta.url)),
-        'Assets': fileURLToPath(new URL('./src/assets', import.meta.url)),
-        'Types': fileURLToPath(new URL('./src/types', import.meta.url)),
-        'vue': 'vue/dist/vue.esm-bundler.js',
-      },
-    },
-    server: process.env.VSCODE_DEBUG
-      ? (() => {
-          const url = new URL(pkg.debug.env.VITE_DEV_SERVER_URL)
-          return {
-            host: url.hostname,
-            port: +url.port,
-          }
-        })()
-      : {
-          host: '127.0.0.1',
-          port: 3000,
-        },
-    build: {
-      target: 'esnext',
-      minify: 'esbuild',
-      sourcemap: sourcemap,
-      chunkSizeWarningLimit: 1000,
-      rollupOptions: {
-        output: {
-          manualChunks: {
-            'vendor': ['vue'],
-          },
-        },
-      },
-    },
-    esbuild: {
-      pure: isDev ? [] : ['console.log', 'console.debug', 'console.info'],
-    },
-  }
-})
+	const isServe = command === 'serve';
+	const isBuild = command === 'build';
+	const isDev = mode === 'development';
+	const sourcemap = isServe || !!process.env.VSCODE_DEBUG;
+
+	return {
+		plugins: [
+			vue(),
+			electron([
+				{
+					// Main-Process entry file of the Electron App.
+					entry: 'electron/main/index.ts',
+					onstart(options) {
+						if (process.env.VSCODE_DEBUG) {
+							console.log('[startup] Electron App');
+						} else {
+							options.startup();
+						}
+					},
+					vite: {
+						build: {
+							sourcemap,
+							minify: isBuild,
+							outDir: path.resolve(__dirname, 'dist-electron/main'),
+							rollupOptions: {
+								external: Object.keys(pkg.dependencies ?? {}),
+							},
+						},
+						optimizeDeps: {
+							exclude: ['electron', 'xmllint-wasm'],
+						},
+					},
+				},
+				{
+					entry: 'electron/preload/index.ts',
+					onstart(options) {
+						// Notify the Renderer-Process to reload the page when the Preload-Scripts build is complete,
+						// instead of restarting the entire Electron App.
+						options.reload();
+					},
+					vite: {
+						build: {
+							sourcemap: sourcemap ? 'inline' : undefined,
+							minify: isBuild,
+							outDir: 'dist-electron/preload',
+							rollupOptions: {
+								external: Object.keys('dependencies' in pkg ? pkg.dependencies : {}),
+							},
+						},
+						optimizeDeps: {
+							exclude: ['electron', 'xmllint-wasm'],
+						},
+					},
+				},
+			]),
+			// Use Node.js API in the Renderer-process
+			renderer({
+				nodeIntegration: true,
+			}),
+		],
+		resolve: {
+			alias: {
+				'@': path.resolve(__dirname, './src'),
+				Views: fileURLToPath(new URL('./src/views', import.meta.url)),
+				Components: fileURLToPath(new URL('./src/components', import.meta.url)),
+				Assets: fileURLToPath(new URL('./src/assets', import.meta.url)),
+				Types: fileURLToPath(new URL('./src/types', import.meta.url)),
+				vue: 'vue/dist/vue.esm-bundler.js',
+			},
+		},
+		server: process.env.VSCODE_DEBUG
+			? (() => {
+					const url = new URL(pkg.debug.env.VITE_DEV_SERVER_URL);
+					return {
+						host: url.hostname,
+						port: +url.port,
+					};
+				})()
+			: {
+					host: '127.0.0.1',
+					port: 3000,
+				},
+		build: {
+			target: 'esnext',
+			minify: 'esbuild',
+			sourcemap: sourcemap,
+			chunkSizeWarningLimit: 1000,
+			rollupOptions: {
+				output: {
+					manualChunks: {
+						vendor: ['vue'],
+					},
+				},
+			},
+		},
+		esbuild: {
+			pure: isDev ? [] : ['console.log', 'console.debug', 'console.info'],
+		},
+	};
+});
