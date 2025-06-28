554b7ecfb741efc7e8f4b0820d89d7dec8faa9f2
marioalvarez32 <33704981+marioalvarez32@users.noreply.github.com>
2025-06-27 21:12:43 -0500

    Improving the experience for uploding files and also introducing some new packages to do xml validation with schemas.

--- src/Client/ReceiptReader/Components/FileUploadDropzone.vue
+++ src/Client/ReceiptReader/Components/FileUploadDropzone.vue
@@ -5,7 +5,9 @@
 			<div class="file-uploader-dropzone__description">
 				<p><span>Arrastra y suelta archivos</span></p>
 				<p>o</p>
-				<v-btn variant="elevated" color="primary" size="small" @click="$emit('open-file-dialog')">Selecciona archivos</v-btn>
+				<v-btn variant="elevated" color="primary" size="small" @click="openDirectoyDialog"
+					>Selecciona archivos</v-btn
+				>
 			</div>
 			<p class="file-uploader-dropzone__supported-files-label">Archivos soportados: XML</p>
 		</div>
@@ -26,127 +28,185 @@
 	</div>
 </template>
 <script setup lang="ts">
-// This component will render a dropzone as a card.
-import type { PropType } from 'vue';
+	import { ref, watch, type PropType } from 'vue';
+	import { ipcRenderer } from 'electron';
+	import FileService from '../Services/FileService';
+	import path from 'path';
 
-const fileListMock = [
-	{
-		name: 'Factura_123.xml',
-		size: '1.2 MB',
-		type: '',
-	},
-	{
-		name: 'Factura_456.xml',
-		size: '800 KB',
-		type: '',
-	},
-	{
-		name: 'Factura_789.xml',
-		size: '2.5 MB',
-		type: '',
-	},
-	{
-		name: 'Factura_1011.xml',
-		size: '1.8 MB',
-		type: '',
-	},
-	{
-		name: 'Factura_1011.xml',
-		size: '1.8 MB',
-		type: '',
-	},
-	{
-		name: 'Factura_1011.xml',
-		size: '1.8 MB',
-		type: '',
-	},
-	{
-		name: 'Factura_1011.xml',
-		size: '1.8 MB',
-		type: '',
-	},
-	{
-		name: 'Factura_1011.xml',
-		size: '1.8 MB',
-		type: '',
-	},
-];
+	const fileService = new FileService();
 
-defineProps({});
+	const fileListMock = [
+		{
+			name: 'Factura_123.xml',
+			size: '1.2 MB',
+			type: '',
+		},
+		{
+			name: 'Factura_456.xml',
+			size: '800 KB',
+			type: '',
+		},
+		{
+			name: 'Factura_789.xml',
+			size: '2.5 MB',
+			type: '',
+		},
+		{
+			name: 'Factura_1011.xml',
+			size: '1.8 MB',
+			type: '',
+		},
+		{
+			name: 'Factura_1011.xml',
+			size: '1.8 MB',
+			type: '',
+		},
+		{
+			name: 'Factura_1011.xml',
+			size: '1.8 MB',
+			type: '',
+		},
+		{
+			name: 'Factura_1011.xml',
+			size: '1.8 MB',
+			type: '',
+		},
+		{
+			name: 'Factura_1011.xml',
+			size: '1.8 MB',
+			type: '',
+		},
+	];
+	const selectedFileDirectory = ref('');
+	const shouldTriggerFileRead = ref(false);
+	const isLoading = ref(false);
+
+	watch(
+		() => shouldTriggerFileRead.value,
+		() => {
+			if (shouldTriggerFileRead.value) {
+				loadFiles();
+				shouldTriggerFileRead.value = false;
+			}
+		},
+	);
+
+	async function loadFiles() {
+		// Read the directoy and get list of files.
+		// Once the files are read, load each one and add it to the fileListMock.
+		const filesInDirectory = ref<string[]>([]);
+		await fileService
+			.readXmlDirectory(selectedFileDirectory.value, 'xml')
+			.then((files) => {
+				filesInDirectory.value = files;
+			})
+			.finally(() => (isLoading.value = false));
+
+		//readFilesInDirectory(filesInDirectory.value);
+		const firstFile = filesInDirectory.value[0];
+		const xmlFilePath = path.join(selectedFileDirectory.value, firstFile);
+		console.log('🚀 ~ loadFiles ~ xmlFilePath:', xmlFilePath);
+
+		fileService.validateCfdi(xmlFilePath);
+		console.log('🚀 ~ loadFiles ~ filesInDirectory:', filesInDirectory);
+	}
+
+	function openDirectoyDialog() {
+		ipcRenderer
+			.invoke('showSelectDirectoryDialog', 'Hello from the renderer!')
+			.then((result) => {
+				if (result.canceled) return;
+				selectedFileDirectory.value = result.filePaths[0];
+				shouldTriggerFileRead.value = true;
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+	}
+
+	function readFilesInDirectory(filesInDirectory: string[]) {
+		isLoading.value = true;
+		fileService
+			.readXmlFiles(selectedFileDirectory.value, filesInDirectory)
+			.then((result) => {
+				console.log('🚀 ~ loadFile ~ result:', result);
+			})
+			.finally(() => (isLoading.value = false));
+	}
 </script>
 
 <style lang="scss" scoped>
-.file-uploader {
-	display: grid;
-	grid-template-columns: 230px 2fr;
-	align-items: center;
-	height: 100%;
-	padding: 25px;
-	gap: 10px;
-}
+	.file-uploader {
+		display: grid;
+		grid-template-columns: 230px 2fr;
+		align-items: center;
+		height: 100%;
+		padding: 25px;
+		gap: 10px;
+	}
 
-.file-uploader-dropzone {
-	height: 100%;
-	width: 100%;
-	//border: 2px dashed rgb(var(--v-theme-primary));
-	border-width: 4px;
-	border-style: dashed;
-	border-radius: 20px; /* For rounded corners */
-	border-color: rgba(var(--v-theme-primary), 0.2);
-	display: flex;
-	justify-content: center;
-	align-items: center;
-	flex-direction: column;
-	gap: 5px;
-}
+	.file-uploader-dropzone {
+		height: 100%;
+		width: 100%;
+		//border: 2px dashed rgb(var(--v-theme-primary));
+		border-width: 4px;
+		border-style: dashed;
+		border-radius: 20px; /* For rounded corners */
+		border-color: rgba(var(--v-theme-primary), 0.2);
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		flex-direction: column;
+		gap: 5px;
+	}
 
-.file-uploader-dropzone__supported-files-label {
-	font-size: 12px;
-}
+	.file-uploader-dropzone__supported-files-label {
+		font-size: 12px;
+	}
 
-.file-uploader-dropzone__description {
-	display: flex;
-	flex-direction: column;
-	align-items: center;
-}
+	.file-uploader-dropzone__description {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+	}
 
-.file-uploader-dropzone__list-container {
-	height: 100%;
-	width: 100%;
-	overflow: hidden;
-	display: flex;
-	flex-direction: column;
-}
+	.file-uploader-dropzone__list-container {
+		height: 100%;
+		width: 100%;
+		overflow: hidden;
+		display: flex;
+		flex-direction: column;
+	}
 
-.file-uploader-dropzone__list-container p {
-	font-weight: bold;
-}
+	.file-uploader-dropzone__list-container p {
+		font-weight: bold;
+	}
 
-.file-uploader-dropzone__list {
-	height: 100%;
-	width: 100%;
-	overflow-y: auto;
-	display: flex;
-	flex-direction: column;
-	gap: 5px;
-	padding-top: 10px;
-}
+	.file-uploader-dropzone__list {
+		height: 100%;
+		width: 100%;
+		overflow-y: auto;
+		display: flex;
+		flex-direction: column;
+		gap: 5px;
+		padding-top: 10px;
+	}
 
-.file-uploader-dropzone__list-item {
-	display: flex;
-	align-items: center;
-	justify-content: space-between;
-	gap: 10px;
-	padding: 5px;
-}
+	.file-uploader-dropzone__list-item {
+		display: flex;
+		align-items: center;
+		justify-content: space-between;
+		gap: 10px;
+		padding: 5px;
+	}
 
-.file-uploader-dropzone__information-container {
-	display: flex;
-	align-items: center;
-}
+	.file-uploader-dropzone__information-container {
+		display: flex;
+		align-items: center;
+	}
 
-.file-uploader-dropzone__file-name {
-	font-weight: bold;
-	font-size: 12px;
-}
+	.file-uploader-dropzone__file-name {
+		font-weight: bold;
+		font-size: 12px;
+	}
 </style>
--- src/Client/ReceiptReader/ReceiptReader.vue
+++ src/Client/ReceiptReader/ReceiptReader.vue
@@ -41,12 +41,27 @@
 			</v-card>
 			<div class="table-header">
 				<div class="table-header__left">
-					<v-select v-model="selectedTableView" :items="tableViews" single-line item-title="label" item-value="view" label="Select" prepend-inner-icon="mdi-format-list-bulleted" density="compact" variant="solo" hide-details></v-select>
+					<v-select
+						v-model="selectedTableView"
+						:items="tableViews"
+						single-line
+						item-title="label"
+						item-value="view"
+						label="Select"
+						prepend-inner-icon="mdi-format-list-bulleted"
+						density="compact"
+						variant="solo"
+						hide-details
+					></v-select>
 				</div>
 
 				<v-dialog width="500" max-height="500">
 					<template v-slot:activator="{ props }">
-						<div v-if="ommittedFiles.length > 0" class="table-header__ommitted-files-container" v-bind="props">
+						<div
+							v-if="ommittedFiles.length > 0"
+							class="table-header__ommitted-files-container"
+							v-bind="props"
+						>
 							<div>{{ ommittedFiles.length }}Recibos omitidos</div>
 							<v-icon icon="mdi-help-circle-outline" color="blue"></v-icon>
 						</div>
@@ -69,13 +84,25 @@
 
 				<div class="table-header__right">
 					<fieldset class="receipt-reader__table-actions">
-						<v-btn :disabled="addedReceipts.length <= 0 || selectedTableView != 'default'" class="table__button" color="blue-grey" size="small" prepend-icon="mdi-export" @click="exportTableToExcel">Exportar</v-btn>
+						<v-btn
+							:disabled="addedReceipts.length <= 0 || selectedTableView != 'default'"
+							class="table__button"
+							color="blue-grey"
+							size="small"
+							prepend-icon="mdi-export"
+							@click="exportTableToExcel"
+							>Exportar</v-btn
+						>
 					</fieldset>
 				</div>
 			</div>
 			<v-card elevation="5" class="table-container">
 				<div rounded="lg" class="receipt-reader__card receipt-reader__table">
-					<ReceiptsTable :added-receipts="addedReceipts" :isExportingData="isExportingData" :selected-table-view="selectedTableView" />
+					<ReceiptsTable
+						:added-receipts="addedReceipts"
+						:isExportingData="isExportingData"
+						:selected-table-view="selectedTableView"
+					/>
 				</div>
 			</v-card>
 		</div>
@@ -83,282 +110,291 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, ref, computed } from 'vue';
-import { readXmlDirectory, readXmlFiles } from './Services/FileService';
-import useIsLoading from './Composables/IsLoading';
-import ReceiptType from '@/Client/ReceiptReader/Models/ReceiptType';
-import { exportReceiptDataToExcel } from './Services/ReceiptReaderService';
-import { ipcRenderer } from 'electron';
-import ReceiptsTable from './Components/ReceiptsTable.vue';
-import FileNameTable from './Components/FileNameTable.vue';
-import FileUploadDropzone from './Components/FileUploadDropzone.vue';
+	import { defineComponent, ref, computed } from 'vue';
+	import FileService from './Services/FileService';
+	import useIsLoading from './Composables/IsLoading';
+	import ReceiptType from '@/Client/ReceiptReader/Models/ReceiptType';
+	import { exportReceiptDataToExcel } from './Services/ReceiptReaderService';
+	import { ipcRenderer } from 'electron';
+	import ReceiptsTable from './Components/ReceiptsTable.vue';
+	import FileNameTable from './Components/FileNameTable.vue';
+	import FileUploadDropzone from './Components/FileUploadDropzone.vue';
 
-export default defineComponent({
-	props: {},
-	components: {
-		ReceiptsTable,
-		FileNameTable,
-		FileUploadDropzone,
-	},
-	setup() {
-		const selectedFileDirectory = ref('');
-		const filesInDirectory = ref<string[]>([]);
-		const addedReceipts = ref<ReceiptType[]>([]);
-		const receiptsTotalAmount = computed<number>(() => addedReceipts.value.reduce((acc, receipt) => acc + receipt.Total, 0));
-		const receiptsSubTotal = computed<number>(() => addedReceipts.value.reduce((acc, receipt) => acc + receipt.SubTotal, 0));
-		const receiptsTaxTotal = computed<number>(() => addedReceipts.value.reduce((acc, receipt) => acc + receipt.TaxAmount, 0));
-		const ommittedFiles = ref<string[]>([]);
+	export default defineComponent({
+		props: {},
+		components: {
+			ReceiptsTable,
+			FileNameTable,
+			FileUploadDropzone,
+		},
+		setup() {
+			const fileService = new FileService();
+			const selectedFileDirectory = ref('');
+			const filesInDirectory = ref<string[]>([]);
+			const addedReceipts = ref<ReceiptType[]>([]);
+			const receiptsTotalAmount = computed<number>(() =>
+				addedReceipts.value.reduce((acc, receipt) => acc + receipt.Total, 0),
+			);
+			const receiptsSubTotal = computed<number>(() =>
+				addedReceipts.value.reduce((acc, receipt) => acc + receipt.SubTotal, 0),
+			);
+			const receiptsTaxTotal = computed<number>(() =>
+				addedReceipts.value.reduce((acc, receipt) => acc + receipt.TaxAmount, 0),
+			);
+			const ommittedFiles = ref<string[]>([]);
 
-		const tableViews = [
-			{ view: 'default', label: 'Vista de recibos' },
-			{ view: 'grouped-by-issuer-RFC', label: 'Agrupados por RFC del emisor' },
-		];
-		const selectedTableView = ref('default');
-		const isExportingData = ref(false);
-		const { isLoading } = useIsLoading();
+			const tableViews = [
+				{ view: 'default', label: 'Vista de recibos' },
+				{ view: 'grouped-by-issuer-RFC', label: 'Agrupados por RFC del emisor' },
+			];
+			const selectedTableView = ref('default');
+			const isExportingData = ref(false);
+			const { isLoading } = useIsLoading();
 
-		function openDirectoyDialog() {
-			ipcRenderer
-				.invoke('showSelectDirectoryDialog', 'Hello from the renderer!')
-				.then((result) => {
-					if (result.canceled) return;
-					selectedFileDirectory.value = result.filePaths[0];
-					scanDirectory();
-				})
-				.catch((err) => {
-					console.log(err);
+			function openDirectoyDialog() {
+				ipcRenderer
+					.invoke('showSelectDirectoryDialog', 'Hello from the renderer!')
+					.then((result) => {
+						if (result.canceled) return;
+						selectedFileDirectory.value = result.filePaths[0];
+						scanDirectory();
+					})
+					.catch((err) => {
+						console.log(err);
+					});
+			}
+
+			function scanDirectory() {
+				isLoading.value = true;
+				fileService
+					.readXmlDirectory(selectedFileDirectory.value, 'xml')
+					.then((files) => {
+						filesInDirectory.value = files;
+					})
+					.finally(() => (isLoading.value = false));
+			}
+
+			function readFiles() {
+				isLoading.value = true;
+				fileService
+					.readXmlFiles(selectedFileDirectory.value, filesInDirectory.value)
+					.then((result) => {
+						addReceipts(result.ReceiptsRead);
+						ommittedFiles.value = result.FilesOmmited;
+					})
+					.finally(() => (isLoading.value = false));
+			}
+
+			function addReceipts(receipts: ReceiptType[]) {
+				receipts.forEach((receipt) => {
+					if (isReceiptAdded(receipt.UUID)) return;
+					addedReceipts.value.push(receipt);
 				});
-		}
 
-		function scanDirectory() {
-			isLoading.value = true;
-			readXmlDirectory(selectedFileDirectory.value, 'xml')
-				.then((files) => {
-					filesInDirectory.value = files;
-				})
-				.finally(() => (isLoading.value = false));
-		}
+				filesInDirectory.value = [];
+			}
 
-		function readFiles() {
-			isLoading.value = true;
-			readXmlFiles(selectedFileDirectory.value, filesInDirectory.value)
-				.then((result) => {
-					addReceipts(result.ReceiptsRead);
-					ommittedFiles.value = result.FilesOmmited;
-				})
-				.finally(() => (isLoading.value = false));
-		}
+			function isReceiptAdded(uuid: string) {
+				return addedReceipts.value.findIndex((receipt) => receipt.UUID == uuid) != -1;
+			}
 
-		function addReceipts(receipts: ReceiptType[]) {
-			receipts.forEach((receipt) => {
-				if (isReceiptAdded(receipt.UUID)) return;
-				addedReceipts.value.push(receipt);
-			});
+			function formatToCurrency(number: number) {
+				return `${number.toLocaleString('en-US', { style: 'currency', currency: 'USD', minimumFractionDigits: 2, maximumFractionDigits: 2 })}`;
+			}
 
-			filesInDirectory.value = [];
-		}
+			function clearReceiptsTable() {
+				addedReceipts.value = [];
+				ommittedFiles.value = [];
+			}
 
-		function isReceiptAdded(uuid: string) {
-			return addedReceipts.value.findIndex((receipt) => receipt.UUID == uuid) != -1;
-		}
+			function exportTableToExcel() {
+				let filePath = null;
+				ipcRenderer
+					.invoke('showSaveFileDialog', 'Hello from the renderer!')
+					.then((result) => {
+						if (result.canceled) return;
+						filePath = result.filePath;
+						isExportingData.value = true;
+						exportReceiptDataToExcel(addedReceipts.value, filePath)
+							.catch((err) => {
+								console.log(err);
+							})
+							.finally(() => (isExportingData.value = false));
+					})
+					.catch((err) => {
+						console.log(err);
+					});
+			}
 
-		function formatToCurrency(number: number) {
-			return `${number.toLocaleString('en-US', { style: 'currency', currency: 'USD', minimumFractionDigits: 2, maximumFractionDigits: 2 })}`;
-		}
-
-		function clearReceiptsTable() {
-			addedReceipts.value = [];
-			ommittedFiles.value = [];
-		}
-
-		function exportTableToExcel() {
-			let filePath = null;
-			ipcRenderer
-				.invoke('showSaveFileDialog', 'Hello from the renderer!')
-				.then((result) => {
-					if (result.canceled) return;
-					filePath = result.filePath;
-					isExportingData.value = true;
-					exportReceiptDataToExcel(addedReceipts.value, filePath)
-						.catch((err) => {
-							console.log(err);
-						})
-						.finally(() => (isExportingData.value = false));
-				})
-				.catch((err) => {
-					console.log(err);
-				});
-		}
-
-		return {
-			openDirectoyDialog,
-			selectedFileDirectory,
-			readFiles,
-			filesInDirectory,
-			isLoading,
-			addedReceipts,
-			receiptsTotalAmount,
-			receiptsSubTotal,
-			receiptsTaxTotal,
-			formatToCurrency,
-			clearReceiptsTable,
-			isExportingData,
-			exportTableToExcel,
-			tableViews,
-			selectedTableView,
-			ommittedFiles,
-		};
-	},
-});
+			return {
+				openDirectoyDialog,
+				selectedFileDirectory,
+				readFiles,
+				filesInDirectory,
+				isLoading,
+				addedReceipts,
+				receiptsTotalAmount,
+				receiptsSubTotal,
+				receiptsTaxTotal,
+				formatToCurrency,
+				clearReceiptsTable,
+				isExportingData,
+				exportTableToExcel,
+				tableViews,
+				selectedTableView,
+				ommittedFiles,
+			};
+		},
+	});
 </script>
 
 <style lang="scss" scoped>
-.receipt-reader__container {
-	margin: 10px 30px;
-	height: calc(100% - 10px);
-	display: flex;
-}
+	.receipt-reader__container {
+		margin: 10px 30px;
+		height: calc(100% - 10px);
+		display: flex;
+	}
 
-.receipt-reader__content {
-	display: grid;
-	gap: 15px;
-	width: 100%;
-	margin-top: 10px;
-	margin-bottom: 20px;
-	grid-template-rows: 85px repeat(11, 1fr);
-	grid-template-columns: repeat(12, 1fr);
-}
+	.receipt-reader__content {
+		display: grid;
+		gap: 15px;
+		width: 100%;
+		margin-top: 10px;
+		margin-bottom: 20px;
+		grid-template-rows: 85px repeat(11, 1fr);
+		grid-template-columns: repeat(12, 1fr);
+	}
 
-.receipt-reader__card {
-	width: 100%;
-	position: relative;
-}
+	.receipt-reader__card {
+		width: 100%;
+		position: relative;
+	}
 
-.receipt-reader__load-directory {
-	display: flex;
-	align-items: center;
-	flex-direction: column;
-	gap: 25px;
-	flex-basis: 40%;
-}
+	.receipt-reader__load-directory {
+		display: flex;
+		align-items: center;
+		flex-direction: column;
+		gap: 25px;
+		flex-basis: 40%;
+	}
 
-.receipt-reader__directory-data {
-	display: flex;
-	justify-content: center;
-	align-items: center;
-	flex-direction: column;
-	gap: 20px;
-	flex-basis: 60%;
-}
+	.receipt-reader__directory-data {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		flex-direction: column;
+		gap: 20px;
+		flex-basis: 60%;
+	}
 
-.receipt-reader__load-directory-content {
-	height: 100%;
-	padding: 20px;
-	display: flex;
-	position: relative;
-}
+	.receipt-reader__load-directory-content {
+		height: 100%;
+		padding: 20px;
+		display: flex;
+		position: relative;
+	}
 
-.receipt-reader__upload-dropzone {
-	grid-row: 1 / span 4;
-	grid-column: 1 / span 6;
-}
+	.receipt-reader__upload-dropzone {
+		grid-row: 1 / span 4;
+		grid-column: 1 / span 6;
+	}
 
-.receipt-reader__input-form {
-	grid-row: 3 / span 5;
-	grid-column: 1 / span 8;
-}
+	.receipt-reader__input-form {
+		grid-row: 3 / span 5;
+		grid-column: 1 / span 8;
+	}
 
-.receipt-reader__input-data {
-	grid-row: 1 / span 4;
-	grid-column: 7 / span 6;
-}
-
-.table-header {
-	grid-row: 5 / span 1;
-	grid-column: 1 / span 12;
-	display: flex;
-	justify-content: space-between;
-	align-items: end;
-}
-
-.table-container {
-	grid-row: 6 / span 8;
-	grid-column: 1 / span 12;
-	height: 100%;
-	overflow: hidden;
+	.receipt-reader__input-data {
+		grid-row: 1 / span 4;
+		grid-column: 7 / span 6;
+	}
 
 	.table-header {
-		height: 50px;
+		grid-row: 5 / span 1;
+		grid-column: 1 / span 12;
 		display: flex;
 		justify-content: space-between;
-		align-items: center;
+		align-items: end;
+	}
 
-		.table__button {
-			font-size: 10px;
+	.table-container {
+		grid-row: 6 / span 8;
+		grid-column: 1 / span 12;
+		height: 100%;
+		overflow: hidden;
+
+		.table-header {
+			height: 50px;
+			display: flex;
+			justify-content: space-between;
+			align-items: center;
+
+			.table__button {
+				font-size: 10px;
+			}
+		}
+		.receipt-reader__table {
+			height: 100%;
+		}
+		.receipt-reader__table-container {
+			display: flex;
+			flex-direction: column;
+			flex-basis: 100%;
+			height: 100%;
+		}
+		:deep(.receipt-reader__table-container .v-table) {
+			height: 100%;
+			width: 100%;
+			overflow: auto;
+		}
+		:deep(.receipt-reader__table-container .v-table .v-table__wrapper) {
+			width: 100%;
+		}
+
+		.receipt-reader__table-actions {
+			display: flex;
+			justify-content: flex-end;
+			gap: 15px;
+			align-items: center;
+			border: none;
 		}
 	}
-	.receipt-reader__table {
-		height: 100%;
-	}
-	.receipt-reader__table-container {
-		display: flex;
-		flex-direction: column;
-		flex-basis: 100%;
-		height: 100%;
-	}
-	:deep(.receipt-reader__table-container .v-table) {
-		height: 100%;
-		width: 100%;
-		overflow: auto;
-	}
-	:deep(.receipt-reader__table-container .v-table .v-table__wrapper) {
-		width: 100%;
-	}
 
-	.receipt-reader__table-actions {
+	.v-overlay {
 		display: flex;
-		justify-content: flex-end;
-		gap: 15px;
+		justify-content: center;
 		align-items: center;
-		border: none;
 	}
-}
 
-.v-overlay {
-	display: flex;
-	justify-content: center;
-	align-items: center;
-}
+	.receipt-reader__data-container {
+		padding: 25px;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		flex-direction: column;
+		gap: 25px;
+	}
 
-.receipt-reader__data-container {
-	padding: 25px;
-	display: flex;
-	justify-content: center;
-	align-items: center;
-	flex-direction: column;
-	gap: 25px;
-}
+	.receipt-reader__files-table {
+		height: 100px;
+		overflow-y: auto;
+		flex-grow: 1;
+		width: 100%;
+		border: 1px solid;
+		border-color: rgba(var(--v-theme-on-background), var(--v-border-opacity));
+	}
 
-.receipt-reader__files-table {
-	height: 100px;
-	overflow-y: auto;
-	flex-grow: 1;
-	width: 100%;
-	border: 1px solid;
-	border-color: rgba(var(--v-theme-on-background), var(--v-border-opacity));
-}
+	.ommitted-files_table {
+		padding: 10px;
+		border: 1px solid;
+		border-color: rgba(var(--v-theme-on-background), var(--v-border-opacity));
+		overflow-y: auto;
+	}
 
-.ommitted-files_table {
-	padding: 10px;
-	border: 1px solid;
-	border-color: rgba(var(--v-theme-on-background), var(--v-border-opacity));
-	overflow-y: auto;
-}
-
-.table-header__ommitted-files-container {
-	justify-content: center;
-	display: flex;
-	gap: 2px;
-	cursor: pointer;
-}
+	.table-header__ommitted-files-container {
+		justify-content: center;
+		display: flex;
+		gap: 2px;
+		cursor: pointer;
+	}
 </style>
